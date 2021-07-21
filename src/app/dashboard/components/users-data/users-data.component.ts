09c4c3c1b958dc3c8593315aadee261a468dce74
MikeYef96 <mike.yef@gmail.com>
2021-07-21 15:52:59 +0300

    added csv download for users-data

--- src/app/dashboard/components/users-data/users-data.component.ts
+++ src/app/dashboard/components/users-data/users-data.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 import { USERS_DATA_TABLE_ADMIN_ARRAY } from '../../constants/user-data-list.constant';
 import {DashboardService} from "../../services/dashboard.service";
 import {IUserData} from "../../model/get-users.model";
+import {ngxCsv} from "ngx-csv";
 
 @Component({
   selector: 'app-users-data',
@@ -21,6 +22,17 @@ export class UsersDataComponent {
 
   constructor(
     public authService: AuthService,
-    private dashboardService: DashboardService
+    private dashboardService: DashboardService,
   ) {}
+
+  onDownload() {
+   this.dataSource  =
+      this.dashboardService.getUsers()
+        .pipe(map((users: IUserData[]) => {
+          new ngxCsv(users, 'My Report');
+          return users
+        }))
+
+    return this.dataSource
+  }
 }
