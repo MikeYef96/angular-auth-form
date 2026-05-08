d0572d3784f7b9104b63f370cacb4af17ce42d13
MikeYef96 <mykhailoyefymovych@teampro.com>
2026-05-08 12:32:46 +0300

    chore: migrate angular v13 to v14

--- src/app/auth/components/sign-in-form/sign-in-form.component.ts
+++ src/app/auth/components/sign-in-form/sign-in-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 import { IAuthState } from 'src/app/auth/model/auth-state.model';
@@ -13,9 +13,9 @@ import { signInRequest } from '../../store/auth.actions';
 export class SignInFormComponent {
   constructor(private store: Store<IAuthState>) {}
 
-  form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.min(4), Validators.required]),
+  form: UntypedFormGroup = new UntypedFormGroup({
+    email: new UntypedFormControl('', [Validators.email, Validators.required]),
+    password: new UntypedFormControl('', [Validators.min(4), Validators.required]),
   });
 
   signInHandler(): void {
